323f4686ba2193b8524435868796990fd8b865f1
NgumtsaB <biolengumtsa@gmail.org>
2024-10-25 08:59:09 +0100

    first commit with maven

---
+++ src/main/java/Practical/work/Unit/testing/and/git/TPTESTU1_NgumtsaCynthia/Tptestu1NgumtsaCynthiaApplication.java
@@ -0,0 +1,13 @@
+package Practical.work.Unit.testing.and.git.TPTESTU1_NgumtsaCynthia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tptestu1NgumtsaCynthiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tptestu1NgumtsaCynthiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Practical/work/Unit/testing/and/git/TPTESTU1_NgumtsaCynthia/Tptestu1NgumtsaCynthiaApplicationTests.java
@@ -0,0 +1,13 @@
+package Practical.work.Unit.testing.and.git.TPTESTU1_NgumtsaCynthia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tptestu1NgumtsaCynthiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
