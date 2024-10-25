aa9c0563a090647fd75d3c564d9a183698506838
NgumtsaB <biolengumtsa@gmail.org>
2024-10-25 11:18:40 +0100

    test analyseSold and getrichest methods

--- TestOperationMathematique/TestUser.java
+++ TestOperationMathematique/TestUser.java
@@ -3,5 +3,9 @@ public class TestUser {
     public void testAddUserInvalidEmail() {
         assertThrows(EmailInvalidException.class, () -> User.add(new User(/* Invalid email */)));
     }
+    @Test
+    public void testAnalyseSoldeGeneral() {
+        assertThrows(NegativeGeneralBalanceException.class, () -> /* Call with negative balance */);
+    }
 
 }
